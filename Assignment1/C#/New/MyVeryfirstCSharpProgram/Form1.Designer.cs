7d2728a31835f2de55cee9325e493e614fe912da
matt <mat.bianchi8@stud.uniroma3.it>
2021-09-29 20:06:36 +0200

    Added new version

---
+++ Assignment1/C#/New/MyVeryfirstCSharpProgram/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace MyVeryfirstCSharpProgram
+{
+    partial class MyFristGUI
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyFristGUI));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button3_MouseClick);
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // MyFristGUI
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "MyFristGUI";
+            this.Load += new System.EventHandler(this.MyFristGUI_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Assignment1/C#/New/MyVeryfirstCSharpProgram/Form1.cs
@@ -0,0 +1,90 @@
+﻿using MyVeryfirstCSharpProgram.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyVeryfirstCSharpProgram
+{
+    public partial class MyFristGUI : Form
+    {
+        public MyFristGUI()
+        {
+            InitializeComponent();
+
+            t.Interval = 4;
+            flag = new Bitmap(480, 160);
+            g = Graphics.FromImage(flag);
+            t.Tick += T_Tick;
+
+        }
+        Bitmap flag;
+        Graphics g;
+        int x = 20;
+        int y = 20;
+        Timer t = new Timer();
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.Text = "button pressed";
+        }
+
+     
+        private void T_Tick(object sender, EventArgs e)
+        {    
+            var rand2 = new Random();
+            g = Graphics.FromImage(flag);
+            g.Clear(Color.White);
+            g.DrawCircle(new Pen(Brushes.Red, 2), x, y, 20);
+            g.FillCircle(Brushes.Red, x, y, 20);
+           
+            x = x+rand2.Next(-10, 10);
+            y = y+ rand2.Next(-10,10);
+            if (x > 460) x = x - 10;
+            if (x < 20) x = x + 10;
+            if (y > 140) y = y - 10;
+            if (y < 20) y = y + 10;
+            pictureBox2.Image = flag;
+
+         
+           
+        }
+
+
+
+        private void MyFristGUI_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            var rand = new Random();
+            x = rand.Next(20, 460);
+            y = rand.Next(20, 140);
+            t.Start();
+            this.button2.Visible = false;
+            this.button3.Text = "STOP";
+            this.button3.Visible = true;
+        }
+
+        private void button3_MouseClick(object sender, MouseEventArgs e)
+        {
+            t.Stop();
+            g.Clear(BackColor);
+            this.button3.Visible = false;
+            this.button2.Visible = true;
+            pictureBox2.Image = flag;
+        }
+
+       
+    }
+}
