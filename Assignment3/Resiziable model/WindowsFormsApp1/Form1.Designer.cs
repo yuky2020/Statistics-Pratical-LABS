2988ef10d20fa96af879271e2d2e6897f0346a21
matt <mat.bianchi8@stud.uniroma3.it>
2021-10-05 20:43:46 +0200

    added 3_A

---
+++ Assignment3/Resiziable model/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.resizaileBox = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resizaileBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.resizaileBox);
+            this.panel1.Location = new System.Drawing.Point(12, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(681, 339);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // resizaileBox
+            // 
+            this.resizaileBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.resizaileBox.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.resizaileBox.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+            this.resizaileBox.Location = new System.Drawing.Point(21, 3);
+            this.resizaileBox.Name = "resizaileBox";
+            this.resizaileBox.Size = new System.Drawing.Size(675, 346);
+            this.resizaileBox.TabIndex = 0;
+            this.resizaileBox.TabStop = false;
+            this.resizaileBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.resizaileBox_MouseDown);
+            this.resizaileBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.resizaileBox_MouseMove);
+            this.resizaileBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.resizaileBox_MouseUp);
+            this.resizaileBox.Resize += new System.EventHandler(this.resizaileBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.resizaileBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox resizaileBox;
+    }
+}
+
---
+++ Assignment3/Resiziable model/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        bool allowResize = false;
+        bool allowMove = false;
+        private void resizaileBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            allowResize = false;
+            allowMove = false;
+        }
+
+        private void resizaileBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (allowResize)
+            {
+                this.panel1.Height = resizaileBox.Top + e.Y;
+                this.panel1.Width = resizaileBox.Left + e.X;
+            }
+            if (allowMove)
+            {
+                this.panel1.Location = new Point(this.panel1.Location.X + e.X, this.panel1.Location.Y + e.Y);
+                
+
+                
+            }
+        }
+
+        private void resizaileBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resizaileBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right) allowResize = true;
+            else allowMove = true;
+        }
+
+       
+
+    }
+}
