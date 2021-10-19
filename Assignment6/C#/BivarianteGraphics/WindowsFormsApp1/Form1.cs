9aaf59056ad875939ae47695e57c4fb268054941
matt <mat.bianchi8@stud.uniroma3.it>
2021-10-19 16:20:33 +0200

    Added 6A

---
+++ Assignment6/C#/BivarianteGraphics/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        Dictionary<int, ElementoDisribuzione> csvContent = new Dictionary<int, ElementoDisribuzione>();
+        Distribuzione distr = new Distribuzione();
+        MediaCalOnline medie = new MediaCalOnline();
+        List<String> attributename = new List<string>();
+        List<String> bivariante = new List<string>();//per ora poi diventera n variante;
+        private void button1_Click(object sender, EventArgs e)
+
+
+        {
+
+            var filePath = string.Empty;
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                filePath = openFileDialog1.FileName;
+
+                //Read the contents of the file into a stream
+                using (TextFieldParser csvParser = new TextFieldParser(filePath))
+                {
+                    csvParser.CommentTokens = new string[] { "#" };
+                    csvParser.SetDelimiters(new string[] { "," });
+                    csvParser.HasFieldsEnclosedInQuotes = true;
+
+                    // Save the row with the column names
+                    string[] fieldsNames = csvParser.ReadFields();
+                    attributename.AddRange(fieldsNames);
+                    int i = 0;
+                    while (!csvParser.EndOfData)
+                    {
+                        // Read current line fields, pointer moves to the next line.
+
+                        string[] fields = csvParser.ReadFields();
+                        ElementoDisribuzione elem = new ElementoDisribuzione(fields[0]);
+                        int j = 0;
+                        foreach (String field in fields)
+                        {
+                            if (!String.IsNullOrEmpty(field))
+                            {
+                                double tmp;
+                                if (Double.TryParse(field, out tmp)) { elem.setVariable(fieldsNames[j], new Tuple<Object, Type>(tmp, tmp.GetType())); medie.addAttribute(fieldsNames[j], tmp); }
+                                else elem.setVariable(fieldsNames[j], new Tuple<Object, Type>(field, field.GetType()));
+                            }
+                            j++;
+                        }
+                        csvContent.Add(i, elem);
+                        distr.addElementoDef(elem);
+                        i++;
+                    }
+                }
+                button2.Visible = true;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ;
+
+            foreach (var elem in attributename)
+                contextMenuStrip1.Items.Add(elem);
+            contextMenuStrip1.Visible = true;
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            richTextBox1.Text += "-------------------------------------------------------------" + Environment.NewLine;
+            richTextBox1.Text += e.ClickedItem.Text + Environment.NewLine;
+
+            double media = 0;
+            double stdVariation;
+            List<Double> values=new List<double>(0);
+            SortedDictionary<Tuple<double, double>, int> firstdistr;
+            medie.getMedia(e.ClickedItem.Text, out media);
+            //se la media è diversa da zero sono sicuro che è double
+            if (media != 0)
+            {
+                Tuple<Object, Type> tmp;
+                foreach (var elm in csvContent.Values)
+                {
+                    elm.getVariable(e.ClickedItem.Text, out tmp);
+
+                    values.Add((double)tmp.Item1);
+
+                }
+                medie.getStandardDeviation(e.ClickedItem.Text, values,out stdVariation);
+                distr.getdistribuzioneN(e.ClickedItem.Text, out firstdistr);
+                foreach (var el in firstdistr)
+                {
+                    richTextBox1.Text += el.Key.ToString() + " : " + el.Value + Environment.NewLine;
+                }
+            }
+            else stdVariation = 0;
+
+
+            richTextBox1.Text += e.ClickedItem + " with average: " + media + " and standard variation: " + stdVariation+ Environment.NewLine;
+            bivariante.Add(e.ClickedItem.Text);
+          
+            button2.Visible = false;
+            foreach (var elem in attributename)
+                contextMenuStrip2.Items.Add(elem);
+            contextMenuStrip2.Visible = true;
+        }
+
+        private void contextMenuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            richTextBox1.Text += "-------------------------------------------------------------" + Environment.NewLine;
+            richTextBox1.Text += e.ClickedItem.Text + Environment.NewLine;
+            double media2 = 0;
+            double stdVariation;
+            List<Double> values = new List<double>(0);
+            SortedDictionary<Tuple<double,double>,int> seconddistr;
+            bool isDouble = false;
+            medie.getMedia(e.ClickedItem.Text, out media2);
+            if (media2 != 0)
+            {
+                isDouble = true;
+                Tuple<Object, Type> tmp;
+                foreach (var elm in csvContent.Values)
+                {
+                    elm.getVariable(e.ClickedItem.Text, out tmp);
+
+                    values.Add((double)tmp.Item1);
+
+                }
+                medie.getStandardDeviation(e.ClickedItem.Text, values, out stdVariation);
+                distr.getdistribuzioneN(e.ClickedItem.Text, out seconddistr);
+                foreach (var el in seconddistr)
+                {
+                    richTextBox1.Text += el.Key.ToString() +" : " + el.Value +Environment.NewLine;
+                }
+            }
+            else stdVariation = 0;
+            richTextBox1.Text += e.ClickedItem + " with average: " + media2 + " and standard variation: " + stdVariation +  Environment.NewLine;
+            bivariante.Add(e.ClickedItem.Text);
+           
+            richTextBox1.Text += Environment.NewLine;
+            int nr, nc;
+            String[,] bivariantMatrix = distr.getbivariantmatrix(bivariante, csvContent.Values, out nr, out nc);
+
+            for (int i = 0; i <= nr; i++)
+            {
+                richTextBox1.Text += Environment.NewLine;
+                for (int j = 0; j <= nc; j++)
+                {
+                    richTextBox1.Text += bivariantMatrix[i, j].PadLeft(6);
+                    
+                }
+            }
+
+        }
+
+
+
+    }
+    }
---
+++ Assignment6/C#/BivarianteGraphics/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Bivarianteform
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Seleziona variabili";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip2.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip2_ItemClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(84, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(755, 681);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 110);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Genera grafici";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(845, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(739, 668);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseLeave += new System.EventHandler(this.pictureBox2_MouseLeave);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
+            // 
+            // Bivarianteform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1587, 750);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Bivarianteform";
+            this.Text = "Statistics Graphics";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
